79a73dfe6b28c0cbeda1365be2b6afc2c3f30066
Martin Vaněk <martin.vanek@vrk.cz>
2016-11-17 15:49:03 +0100

    Ray tracer breakdown to smaller methods

--- zpg_pg1/zpg/RayTracer.cpp
+++ zpg_pg1/zpg/RayTracer.cpp
@@ -20,6 +20,55 @@ float RayTracer::isInShadow(Vector3 position, Vector3 lightVector) const
 	return shadowRay.isCollided() ? 0.f : 1.f;
 }
 
+Color4 RayTracer::transmited_color(RayPayload& load, uint nest, Vector3 rd, Color4 reflected_trace, const Material* materialBefore) const
+{
+	//float cos_theta2 = (load.normal).dot(-rd);
+	load.normal.normalize();
+	rd.normalize();
+	float n1 = materialBefore->ior;
+	float n2 = load.material->ior;
+	if (materialBefore->get_name() == "green_plastic_transparent")
+	{
+		n2 = 1;
+	}
+	float cos_theta2 = (load.normal).dot(-rd);
+	if (cos_theta2 < 0)
+	{
+		load.normal = -load.normal;
+		cos_theta2 = (load.normal).dot(-rd);
+	}
+
+	float cos_thetai_sqrt = 1 - SQR(n1 / n2) * (1 - SQR(cos_theta2));
+	if (cos_thetai_sqrt < 0)
+	{
+		//wtf to do in total reflection
+		cos_thetai_sqrt = 0.6;
+	}
+	float cos_theta1 = sqrt(cos_thetai_sqrt);
+	Vector3 rr = -(n1 / n2) * rd - ((n1 / n2) * cos_theta2 + cos_theta1) * load.normal;
+	rr.normalize();
+
+	Vector3 l = rr - (2 * (load.normal.dot(rr))) * load.normal;
+	Vector3 lr = -l;
+	Ray transmitedRay(load.position, lr, 0.01f);
+	Color4 transmitedColor = this->trace(transmitedRay, nest - 1, load.material);
+
+	float rs = std::pow((n1 * cos_theta1 - n2 * cos_theta2) / (n1 * cos_theta1 + n2 * cos_theta2), 2);
+	float rp = std::pow((n1 * cos_theta2 - n2 * cos_theta1) / (n1 * cos_theta2 + n2 * cos_theta1), 2);
+	float reflectivity = 0.5f * (rs + rp);
+	float transmitivity = 1 - reflectivity;
+	return load.ambient_color +
+		transmitivity * transmitedColor * load.material->diffuse +
+		reflectivity * reflected_trace * load.material->get_reflexivity();
+}
+
+Color4 RayTracer::reflectedColor(RayPayload load, uint nest, Vector3 rd, Color4& reflected_trace) const
+{
+	Vector3 reflectedRayDirection = (rd).reflect(load.normal);
+	Ray reflectedRay = Ray(load.position, reflectedRayDirection, 0.01f);
+	return this->trace(reflectedRay, nest - 1, load.material);
+}
+
 Color4 RayTracer::trace(Ray& ray, uint nest, const Material* materialBefore) const
 {
 	rtcIntersect(scene, ray);
@@ -27,72 +76,29 @@ Color4 RayTracer::trace(Ray& ray, uint nest, const Material* materialBefore) con
 	if (ray.isCollided())
 	{
 		Vector3 rd = ray.direction().normalize();
-		Vector3 lightVector = load.light_vector();
-		float inShadow = isInShadow(load.position, lightVector);
-		lightVector.normalize();
+		Vector3 lightVector = load.light_vector().normalize();
 
-		Color4 diffuse = load.diffuse_color * std::max(lightVector.dot(load.normal), 0.f);
-
-
-		Vector3 halfVector = load.half_vector().normalize();
-
-		Color4 specular = load.specular_color * std::pow(std::max(halfVector.dot(load.normal), 0.f), 2);
-		Color4 reflected_trace = Color4(0.f);
-		float diffuseScale = (1 - load.material->reflectivity);
-
+		Color4 reflected_color = Color4(0.f);
 		if (nest > 0)
 		{
-			Vector3 reflectedRayDirection = (rd).reflect(load.normal);
-			Ray reflectedRay = Ray(load.position, reflectedRayDirection, 0.01f);
-			reflected_trace = this->trace(reflectedRay, nest - 1, load.material);
+			reflected_color = reflectedColor(load, nest, rd, reflected_color);
 
 			if (load.material->get_name() == "green_plastic_transparent")
 			{
-				//float cos_theta2 = (load.normal).dot(-rd);
-				load.normal.normalize();
-				rd.normalize();
-				float n1 = materialBefore->ior;
-				float n2 = load.material->ior;
-				if (materialBefore->get_name() == "green_plastic_transparent")
-				{
-					n2 = 1;
-				}
-				float cos_theta2 = (load.normal).dot(-rd);
-				if (cos_theta2 < 0)
-				{
-					load.normal = -load.normal;
-					cos_theta2 = (load.normal).dot(-rd);
-				}
-
-				float cos_thetai_sqrt = 1 - SQR(n1 / n2) * (1 - SQR(cos_theta2));
-				if (cos_thetai_sqrt < 0)
-				{
-					//wtf to do in total reflection
-					cos_thetai_sqrt = 0.6;
-				}
-				float cos_theta1 = sqrt(cos_thetai_sqrt);
-				Vector3 rr = -(n1 / n2) * rd - ((n1 / n2) * cos_theta2 + cos_theta1) * load.normal;
-				rr.normalize();
-
-				Vector3 l = rr - (2 * (load.normal.dot(rr))) * load.normal;
-				Vector3 lr = -l;
-				Ray transmitedRay(load.position, lr, 0.01f);
-				Color4 transmitedColor = this->trace(transmitedRay, nest - 1, load.material);
-
-				float rs = std::pow((n1 * cos_theta1 - n2 * cos_theta2) / (n1 * cos_theta1 + n2 * cos_theta2), 2);
-				float rp = std::pow((n1 * cos_theta2 - n2 * cos_theta1) / (n1 * cos_theta2 + n2 * cos_theta1), 2);
-				float reflectivity = 0.5f * (rs + rp);
-				float transmitivity = 1 - reflectivity;
-				return load.ambient_color +
-					transmitivity * transmitedColor * load.material->diffuse +
-					reflectivity * reflected_trace * load.material->get_reflexivity();
+				return transmited_color(load, nest, rd, reflected_color, materialBefore);
 			}
 		}
+		Color4 diffuse = load.diffuse_color * std::max(lightVector.dot(load.normal), 0.f);
 
+		Vector3 halfVector = load.half_vector().normalize();
+		Color4 specular = load.specular_color * std::pow(std::max(halfVector.dot(load.normal), 0.f), 2);
+		float diffuseScale = (1 - load.material->reflectivity);
+
+		float inShadow = isInShadow(load.position, lightVector);
 
 		return load.ambient_color +
 			inShadow * diffuse * diffuseScale +
-			specular * load.material->reflectivity * reflected_trace;
+			specular * load.material->reflectivity * reflected_color;
 	}
 	else
 	{
--- zpg_pg1/zpg/RayTracer.h
+++ zpg_pg1/zpg/RayTracer.h
@@ -10,6 +10,8 @@ public:
 
 	Color4 trace(Ray& ray, uint nest) override;
 	float isInShadow(Vector3 position, Vector3 lightVector) const;
+	Color4 transmited_color(RayPayload& load, uint nest, Vector3 rd, Color4 reflected_trace, const Material* materialBefore) const;
+	Color4 reflectedColor(RayPayload load, uint nest, Vector3 rd, Color4& reflected_trace) const;
 	Color4 trace(Ray& ray, uint nest, const Material* material) const;
 	virtual ~RayTracer();
 };
