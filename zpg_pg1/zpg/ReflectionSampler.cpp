5178e27a11e3caefd98a841eb57237ae4f1221d3
Martin Vaněk <martin.vanek@vrk.cz>
2016-12-06 21:56:35 +0100

    Reflection sampler without dot product.

--- zpg_pg1/zpg/ReflectionSampler.cpp
+++ zpg_pg1/zpg/ReflectionSampler.cpp
@@ -8,19 +8,20 @@ Color4 ReflectionSampler::fr()
 std::tuple<Color4, Vector3> ReflectionSampler::sample(const Vector3& incoming_direction,
 	const Vector3& normal, const Color4& diffuse_color)
 {
+	Vector3 reflect = Vector3(incoming_direction).normalize();
 	Vector3 reflected_direction;
 	
-	if(incoming_direction.dot(normal) < 0)
+	if(reflect.dot(normal) < 0)
 	{
-		reflected_direction = incoming_direction.reflect(-normal).normalize();
+		reflected_direction = reflect.reflect(-normal).normalize();
 	}else
 	{
-		reflected_direction = incoming_direction.reflect(normal).normalize();
+		reflected_direction = reflect.reflect(normal).normalize();
 	}
 	
 
-
+	//fr() *
 	float dot = reflected_direction.dot(normal);
-	Color4 color = fr() * dot;
+	Color4 color =  Color4(dot);
 	return std::make_tuple(color, reflected_direction);
 }
